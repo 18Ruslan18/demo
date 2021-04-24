c3e5b911caf90017ea34ab28ff06d7eb625ec19c
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-04-24 15:06:47 +0300

    add message form

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,40 @@
 var messageApi = Vue.resource('/message{/id}');
+Vue.component('message-form',
+    {
+        props:['messages'],
+        data: function (){
+            return{
+                text: ''
+            }
+
+        },
+        template: '<div>' +
+            '<input type="text" placeholder="Write something" v-model="text" />' + //link variable text
+            '<input type="button" value="Save" @click="save"/>' +
+            //<input type="button" value="Save" @click="save"/>'
+            '</div> ',
+        methods:{
+            save: function (){
+                var message = {text: this.text};
+                messageApi.save({},message).then(result=>
+                result.json().then(data=>{
+                    this.messages.push(data);
+                    this.text=''
+                }))
+            }
+        }
+    }
+
+)
 Vue.component('message-row', {
     props:['message'],
     template: '<div><i>({{ message.id }})</i>{{message.text}}</div>'
 });
 Vue.component('messages-list', {
     props:['messages'],
-    template: '<div><message-row  v-for="message in messages" :key="message.id" :message="message"/></div>',
+    template: '<div>' +
+        '<message-form :messages="messages"/>' +
+        '<message-row  v-for="message in messages" :key="message.id" :message="message"/></div>',
     created: function(){
         messageApi.get().then(result=>
             result.json().then(data=>
